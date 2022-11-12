3ff31b842fb3592fcd02894bfc61894c36718ba7
Rafael Lourenco <52457167+Rafasputnick@users.noreply.github.com>
2022-11-12 17:48:04 -0300

    feat: get byte using bitwise operators

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
+
+int getBit(uint8_t byte, int bit) {
+    uint8_t shefittedByte = (byte >> (8 - bit ) );
+    return (shefittedByte & 1);
+}
 
 int main() {
-    int x = 10;
-    printf("oloko\n");
-    printf("%d\n", x);
+    uint8_t x = 1;
+    printf("%d\n", getBit(x, 8));
 }

1c7aa3ccbb8854cfd14bc8a4bc32bbe2b3a545ec
Rafael Lourenco <52457167+Rafasputnick@users.noreply.github.com>
2022-11-12 19:27:44 -0300

    feat: getting arguments from command line

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int getBit(uint8_t byte, int bit) {
-    uint8_t shefittedByte = (byte >> (8 - bit ) );
-    return (shefittedByte & 1);
+  uint8_t bitsAtRigth =  bit - 1;
+  uint8_t shefittedByte = byte >> bitsAtRigth;
+  return shefittedByte & 1;
 }
 
-int main() {
-    uint8_t x = 1;
-    printf("%d\n", getBit(x, 8));
+int main(int argc, const char *argv[]) {
+  printf("%d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+
+  exit(0);
 }
